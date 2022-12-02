d2eeeb6f121d67aa47fe022b7dcbe1932ae95931
ajaysingh8462 <ajaysingh84625@gmail.com>
2022-12-02 17:43:10 +0530

    [AJAY]- Player Position

---
+++ conditions/conditions/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter Number");
+int num=Convert.ToInt32(Console.ReadLine());
+for(int i=1;i<=num; i++)
+{
+    Console.WriteLine("Number" + +i);
+}
+
+int k=1;
+while(k<=10)
+{
+    Console.WriteLine("k" +k);
+    k++;
+}
+
+int a = 1;
+while (a <= 10)
+{
+    Console.WriteLine("a" + a);
+    a++;
+}
---
+++ hello/hello/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello What is your Name");
+                String name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+            Console.ReadLine(); 
+        }
+    }
+}
